5397240608d77ab23c5ef61827037df97691938b
Ramon Nascimento <ramonnascimento@Ramons-Mac.local>
2021-08-04 16:08:07 -0300

    Consider gift, promotion, only one item in cart

--- massive_partnerintegrationbra.js
+++ massive_partnerintegrationbra.js
@@ -1,9 +1,12 @@
 const accountName = 'partnerintegrationbra';
-const n = 60;
-const VtexIdclientAutCookie = "Adicionar Cookie"
+const n = 20;
+const VtexIdclientAutCookie = "Coloque aqui o Token"
+const itemsList = [
+    "1",
+    "6"
+]
 
 const axios = require('axios');
-
 const instance = axios.create({
     headers: {
         VtexIdclientAutCookie,
@@ -12,17 +15,25 @@ const instance = axios.create({
     }
 })
 
-let orderPut = {
-    "items": [{
-        "id": "1",
-        "quantity": 1,
-        "seller": "1",
-        "price": 12200,
-        "rewardValue": 0,
-        "offerings": [],
-        "priceTags": [],
-        "isGift": false
-    }],
+function generateRandomIntegerInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const itemSimulation = id =>
+({
+    id,
+    "quantity": 1,
+    "seller": "1"
+});
+
+const simulationRequest = (itemsSimulation) => ({
+    "items": itemsSimulation,
+    "postalCode": "22270-030",
+    "country": "BRA"
+});
+
+const orderPut = (items,logisticsInfo,totalPrice) => ({
+    items,
     "clientProfileData": {
         "email": "teste@teste.com",
         "firstName": "Teste",
@@ -42,11 +53,7 @@ let orderPut = {
         "address": {
             "addressId": "4835155269459",
         },
-        "logisticsInfo": [{
-            "itemIndex": 0,
-            "selectedSla": "jatinho",
-            "price": 500
-        }]
+        logisticsInfo
     },
     "paymentData": {
         "id": "paymentData",
@@ -54,21 +61,21 @@ let orderPut = {
             "accountId": "1A7E349D1CA549D189BF4A0062A4B451",
             "bin": "424242",
             "paymentSystem": "2",
-            "referenceValue": 12700,
-            "value": 12700,
+            "referenceValue": totalPrice,
+            "value": totalPrice,
             "installments": 1
         }]
     }
-}
+})
 
-const orderPayment = (transactionId) => [
+const orderPayment = (transactionId, totalPrice) => [
     {
         "hasDefaultBillingAddress": true,
         "installmentsInterestRate": 0,
-        "referenceValue": 12700,
+        "referenceValue": totalPrice,
         "bin": "424242",
         "accountId": "1A7E349D1CA549D189BF4A0062A4B451",
-        "value": 12700,
+        "value": totalPrice,
         "tokenId": null,
         "paymentSystem": "2",
         "isBillingAddressDifferent": false,
@@ -84,12 +91,12 @@ const orderPayment = (transactionId) => [
         "chooseToUseNewCard": false,
         "id": `${accountName.toUpperCase()}`,
         "interestRate": 0,
-        "installmentValue": 12700,
+        "installmentValue": totalPrice,
         "transaction": {
             "id": `${transactionId}`,
             "merchantName": `${accountName.toUpperCase()}`
         },
-        "installmentsValue": 12700,
+        "installmentsValue": totalPrice,
         "currencyCode": "BRL",
         "originalPaymentIndex": 0,
         "groupName": "creditCardPaymentGroup"
@@ -98,16 +105,53 @@ const orderPayment = (transactionId) => [
 const orders = [];
 
 for (let i = 0; i < n; i++) {
-    orders.push(createOrder())
+    const itemsSimulation = [];
+    for (let j=0; j<5; j++){
+        itemsSimulation.push(itemSimulation(itemsList[generateRandomIntegerInRange(0,itemsList.length-1)]))
+    }
+    orders.push(createOrder(simulationRequest(itemsSimulation)))
 }
 
 Promise.all(orders);
 
-async function createOrder() {
+async function createOrder(simulationRequest) {
     try {
         //Criar pedido
-        const orderResponse = await instance.put(`https://${accountName}.myvtex.com/api/checkout/pub/orders?sc=1`, orderPut);
+        const simulationResponse = await instance.post(`https://${accountName}.myvtex.com/api/checkout/pvt/orderforms/simulation?sc=1`, simulationRequest);
+        const items = []
+        const simulationItems = simulationResponse.data.items
+        simulationItems.forEach(item => {
+            const sku = {
+                "id": item.id,
+                "quantity": item.quantity,
+                "seller": "1",
+                "sellerChain": item.sellerChain,
+                "price": item.price,
+                "rewardValue": 0,
+                "offerings": [],
+                "priceTags": [],
+                "isGift": false
+            } 
+            items.push(sku)
+        });
+
+        const logisticInfo=[]
+        const simulationLogistic = simulationResponse.data.logisticsInfo
+        simulationLogistic.forEach(element => {
+            logisticInfo.push({
+                "itemIndex": element.itemIndex,
+                "selectedSla": element.slas[0].id,
+                "price": element.slas[0].price
+            })
+        })
         
+        let totalPrice = 0;
+        items.forEach(element => totalPrice = totalPrice + element.price*element.quantity)
+        logisticInfo.forEach(element => totalPrice = totalPrice + element.price)
+        
+
+        const orderResponse = await instance.put(`https://${accountName}.myvtex.com/api/checkout/pub/orders?sc=1`, orderPut(items, logisticInfo, totalPrice));
+
         const headers = orderResponse.headers['set-cookie'];
         let Vtex_CHKO_Auth;
         headers.forEach(cookie => {
@@ -119,15 +163,15 @@ async function createOrder() {
         const orderId = orderResponse.data.orders[0].orderGroup;
 
         //Criar transação
-        await instance.post(`https://${accountName}.vtexpayments.com.br/api/pub/transactions/${transactionId}/payments`, orderPayment(transactionId));
-        
+        await instance.post(`https://${accountName}.vtexpayments.com.br/api/pub/transactions/${transactionId}/payments`, orderPayment(transactionId, totalPrice));
+
         //Processar pagamento
         await instance.post(`https://${accountName}.myvtex.com/api/checkout/pub/gatewayCallback/${orderId}`, {}, {
             headers: {
                 'Cookie': `Vtex_CHKO_Auth=${Vtex_CHKO_Auth}`
             }
         })
-        
+
         console.log('Ordem criada: ' + `${orderId}`);
     }
     catch (err) {
