53d8b8d2048c6c99602a9c1ca437c277b5820a70
Ramon Nascimento <ramonnascimento@Ramons-Mac.local>
2021-08-04 18:57:41 -0300

    Remove Cookie from script

--- massive_avon.js
+++ massive_avon.js
@@ -1,9 +1,58 @@
-const accountName = 'partnerintegrationbra';
-const n = 20;
-const VtexIdclientAutCookie = "Coloque aqui o Token"
+const accountName = 'avonqa';
+const n = 1;
+const VtexIdclientAutCookie = "Adicionar Cookie"
 const itemsList = [
-    "1",
-    "6"
+    '76176123',
+    '76173458',
+    '119231204',
+    '119231210',
+    '76175278',
+    '83391634',
+    '76175175',
+    '90917754',
+    '76173202',
+    '81944833',
+    '76175698',
+    '119231214',
+    '99080720',
+    '76173680',
+    '119231207',
+    '76376809',
+    '119231205',
+    '119231202',
+    '119231208',
+    '119231211',
+    '119231212',
+    '104385234',
+    '76177733',
+    '76176151',
+    '79288370',
+    '99990479',
+    '119231213',
+    '119231201',
+    '76175960',
+    '76178591',
+    '119231218',
+    '119231200',
+    '76177162',
+    '78517870',
+    '78517689',
+    '76175719',
+    '119231206',
+    '119231216',
+    '76174620',
+    '119231215',
+    '76176622',
+    '76176739',
+    '82966246',
+    '76176717',
+    '76176622',
+    '79160760',
+    '76172982',
+    '82966246',
+    '76173034',
+    '76173057',
+    '76176622'
 ]
 
 const axios = require('axios');
@@ -28,19 +77,19 @@ const itemSimulation = id =>
 
 const simulationRequest = (itemsSimulation) => ({
     "items": itemsSimulation,
-    "postalCode": "22270-030",
+    "postalCode": "04272-300",
     "country": "BRA"
 });
 
-const orderPut = (items,logisticsInfo,totalPrice) => ({
+const orderPut = (items, logisticsInfo, totalPrice) => ({
     items,
     "clientProfileData": {
-        "email": "teste@teste.com",
-        "firstName": "Teste",
-        "lastName": "Teste",
-        "document": "39033658828",
+        "email": "bruna.baldaconi@avon.com",
+        "firstName": "Bruna",
+        "lastName": "Baldaconi",
+        "document": "38417853871",
         "documentType": "cpf",
-        "phone": "+5521988554477",
+        "phone": "+5511983585555",
         "corporateName": null,
         "tradeName": null,
         "corporateDocument": null,
@@ -51,16 +100,16 @@ const orderPut = (items,logisticsInfo,totalPrice) => ({
     "shippingData": {
         "id": "shippingData",
         "address": {
-            "addressId": "4835155269459",
+            "addressId": "1625680876285",
         },
         logisticsInfo
     },
     "paymentData": {
         "id": "paymentData",
         "payments": [{
-            "accountId": "1A7E349D1CA549D189BF4A0062A4B451",
-            "bin": "424242",
-            "paymentSystem": "2",
+            "accountId": "4F18E88303B1494BAF41809A443E8F6B",
+            "bin": "518745",
+            "paymentSystem": "4",
             "referenceValue": totalPrice,
             "value": totalPrice,
             "installments": 1
@@ -73,18 +122,18 @@ const orderPayment = (transactionId, totalPrice) => [
         "hasDefaultBillingAddress": true,
         "installmentsInterestRate": 0,
         "referenceValue": totalPrice,
-        "bin": "424242",
-        "accountId": "1A7E349D1CA549D189BF4A0062A4B451",
+        "bin": "518745",
+        "accountId": "4F18E88303B1494BAF41809A443E8F6B",
         "value": totalPrice,
         "tokenId": null,
-        "paymentSystem": "2",
+        "paymentSystem": "4",
         "isBillingAddressDifferent": false,
         "fields": {
             "dueDate": null,
-            "validationCode": "123",
-            "bin": "424242",
-            "accountId": "1A7E349D1CA549D189BF4A0062A4B451",
-            "addressId": "4835155269459",
+            "validationCode": "980",
+            "bin": "518745",
+            "accountId": "4F18E88303B1494BAF41809A443E8F6B",
+            "addressId": "1625680876285",
             "cardNumber": null
         },
         "installments": 1,
@@ -105,10 +154,18 @@ const orderPayment = (transactionId, totalPrice) => [
 const orders = [];
 
 for (let i = 0; i < n; i++) {
-    const itemsSimulation = [];
-    for (let j=0; j<5; j++){
-        itemsSimulation.push(itemSimulation(itemsList[generateRandomIntegerInRange(0,itemsList.length-1)]))
+    let itemsSimulation = [];
+    const seen = new Set();
+
+    for (let j = 0; j < 5; j++) {
+        itemsSimulation.push(itemSimulation(itemsList[generateRandomIntegerInRange(0, itemsList.length - 1)]))
     }
+    //console.log(itemsSimulation)
+    itemsSimulation = itemsSimulation.filter(el => {
+        const duplicate = seen.has(el.id);
+        seen.add(el.id);
+        return !duplicate;
+      });
     orders.push(createOrder(simulationRequest(itemsSimulation)))
 }
 
@@ -117,25 +174,25 @@ Promise.all(orders);
 async function createOrder(simulationRequest) {
     try {
         //Criar pedido
+        //console.log(simulationRequest)
         const simulationResponse = await instance.post(`https://${accountName}.myvtex.com/api/checkout/pvt/orderforms/simulation?sc=1`, simulationRequest);
         const items = []
         const simulationItems = simulationResponse.data.items
-        simulationItems.forEach(item => {
+        simulationItems.forEach((item) => {
             const sku = {
                 "id": item.id,
-                "quantity": item.quantity,
+                "quantity": "1",
                 "seller": "1",
-                "sellerChain": item.sellerChain,
-                "price": item.price,
+                "price": item.sellingPrice,
                 "rewardValue": 0,
                 "offerings": [],
-                "priceTags": [],
-                "isGift": false
-            } 
+                "isGift": item.requestIndex !== null ? false : true
+            }
             items.push(sku)
         });
+        //console.log(items);
 
-        const logisticInfo=[]
+        const logisticInfo = []
         const simulationLogistic = simulationResponse.data.logisticsInfo
         simulationLogistic.forEach(element => {
             logisticInfo.push({
@@ -144,11 +201,13 @@ async function createOrder(simulationRequest) {
                 "price": element.slas[0].price
             })
         })
-        
+        //console.log(logisticInfo)
         let totalPrice = 0;
-        items.forEach(element => totalPrice = totalPrice + element.price*element.quantity)
+        simulationResponse.data.totals.forEach(item => {
+            totalPrice = totalPrice + item.value
+        });
         logisticInfo.forEach(element => totalPrice = totalPrice + element.price)
-        
+
 
         const orderResponse = await instance.put(`https://${accountName}.myvtex.com/api/checkout/pub/orders?sc=1`, orderPut(items, logisticInfo, totalPrice));
 
